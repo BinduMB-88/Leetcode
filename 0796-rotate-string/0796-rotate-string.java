3f1fce89bf30d26b8541b0259de1e520a9cad351
Bindu M B <bindubhadragoudra4@gmail.com>
2026-09-07 15:58:01 +0530

    Time: 2 ms (64.09%), Space: 43.3 MB (82.38%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()) return false;
+        String m=s+s;
+         return m.contains(goal);
+    }
+}
